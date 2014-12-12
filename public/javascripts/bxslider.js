a9e4f89fde4be3044880984ff58a1e43478cc62f
maninigupta <manini.gupta@gmail.com>
2014-12-12 17:52:37 -0500

    fix bxslider and image carousel works

--- public/javascripts/bxslider.js
+++ public/javascripts/bxslider.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-  $('.bxslider').bxSlider();
+  $('.bxslider').bxSlider({
+    auto: true,
+    controls: false,
+    pause: 3000,
+    mode: "fade",
+    speed: 800
+  });
 });
