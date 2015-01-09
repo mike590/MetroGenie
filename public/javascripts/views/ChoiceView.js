0c13152b8d185b6d737d5f3a8922e25b5a424a00
mike590 <mike.gherard@gmail.com>
2015-01-09 15:03:42 -0500

    Removed tagName and className from ChoiceView. Gave appropritate class names to each template and colored them differently in styles.css.

--- public/javascripts/views/ChoiceView.js
+++ public/javascripts/views/ChoiceView.js
@@ -13,8 +13,6 @@ var ChoiceView = Backbone.View.extend({
         return _.template($('#hybrid-choice-template').html())
     }
   },
-  tagName: 'span',
-  className: "choice",
   render: function(){
     this.$el.html(this.template()(this.model.toJSON()));
     return this;
