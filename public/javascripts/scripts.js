158da79bf5547c23a4981000828cff89785cde4d
mike590 <mike.gherard@gmail.com>
2015-01-12 19:05:29 -0500

    Working around with questions row

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -47,7 +47,15 @@ $(function(){
     }
   });
 });
-  
+
+// $(document).scroll(function(){
+//     if($(this).scrollTop() > t)
+//     {   
+//         $('#voice2').css({"border-bottom":"2px solid #f4f5f8"});
+//         $('#voice3').css({"border-bottom":"2px solid #2e375b"});
+//     }
+// });
+
 function submitData(){
   console.log("submit data");
   $newCard = $('input:checked')
